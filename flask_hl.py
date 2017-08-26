6d97b88890cf47cfe65b44b1a954bd39bf90a7eb
余浩鹏 <hp@MacBook-Air-2.local>
2017-08-26 22:48:03 +0800

    添加session,实现登陆登出

--- flask_hl.py
+++ flask_hl.py
@@ -1,25 +1,38 @@
 # -*- coding: utf-8 -*-
 import os
-from flask import Flask, redirect, url_for, render_template, request, abort
+from flask import Flask, redirect, url_for, render_template, request, abort, session, abort
 
 app = Flask(__name__)
 
 app.config.update(dict(
     USERNAME = 'admin',
-    PASSWORD = '123'
+    PASSWORD = '123',
+    SECRET_KEY = 'key'
 ))
 
 @app.route('/')
 def index():
-    return "Hello,World"
+    if  not session.get('logged_in'):
+        message = "world"
+    else:
+        message = "admin"
+    return render_template("index.html", message=message)
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        if request.form["username"] != app.config["USERNAME"] and request.form["password"] != app.config["PASSWORD"]:
+        if request.form["username"] != app.config["USERNAME"] or request.form["password"] != app.config["PASSWORD"]:
             abort(401)
+        else:
+            session['logged_in'] = True
+            return redirect(url_for("index"))
     return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for("index"))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
