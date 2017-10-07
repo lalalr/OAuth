e2f03b527f7a627fe75a34ccbcede621cd9c2892
余浩鹏 <hp@MacBook-Air-2.local>
2017-10-07 16:32:20 +0800

    fixed

--- flask_hl.py
+++ flask_hl.py
@@ -23,15 +23,21 @@ class User(Base):
     __tablename__ = 'users'
     user_id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(VARCHAR(16), unique=True)
-    password = Column(VARCHAR(32))
+    password = Column(VARCHAR(128))
 
     def __init__(self, username, plaintext_password):
         self.username = username
-        self.password = plaintext_password
+        self.set_pw(plaintext_password)
 
     def __repr__(self):
         return "<users(user_id='%s', username='%s', password='%s')>" % (
             self.user_id, self.username, self.password)
+
+    def set_pw(self, plaintext_password):
+        self.password = generate_password_hash(plaintext_password)
+
+    def check_pw(self, plaintext_password):
+        return check_password_hash(self.password, plaintext_password)
 
 @app.route('/')
 def index():
@@ -45,10 +51,10 @@ def index():
 def login():
     error=None
     if request.method == "POST":
-        user = sess.query(User).filter(User.username == request.form['username'])
-        if user is None:
+        query = sess.query(User).filter(User.username == request.form['username'])
+        if query.count() == 0:
             error = "账号错误"
-        elif not User.verify_password(request.form['passworld']):
+        elif not query.first().check_pw(request.form['password']):
             error = "密码错误"
         else:
             session['logged_in'] = True
@@ -70,7 +76,7 @@ def register():
             error = "密码为空"
         elif request.form['password'] != request.form['password2']:
             error = "与上面的密码不符"
-        elif sess.query(User).filter(User.username == request.form['username']).first() is None:
+        elif sess.query(User).filter(User.username == request.form['username']).count() != 0:
             error = "账号名已存在"
         else:
             new_user = User(request.form['username'], request.form['password'])
